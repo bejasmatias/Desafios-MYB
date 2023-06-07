82b682da1440f33c3fca6003bc4162115362084f
Matias Bejas <bejasmatias@gmail.com>
2023-06-07 02:07:08 -0300

    adding validations, solving calendar issue

--- Clases/Clase6-Desafio3/script.js
+++ Clases/Clase6-Desafio3/script.js
@@ -7,27 +7,65 @@ function validateForm(event) {
     const password = document.getElementById("password").value;
     const confirmPassword = document.getElementById("confirm-password").value;
     const birthday = document.getElementById("birthdate").value;
-
-
+    const newsletterCheckbox = document.getElementById("newsletter-checkbox");
+    const termsCheckbox = document.getElementById("terms-checkbox");
+  
+    // Validación de campos
     if (!firstName || !lastName || !username || !password || !confirmPassword || !birthday) {
       showError("Please fill in all fields.");
       return;
     }
   
-
+    // Validación de contraseñas
     if (password !== confirmPassword) {
       showError("Passwords do not match.");
       return;
     }
-
+  
+    // Validación de caracteres especiales en campos de texto
     if (!/^[\w\s]+$/.test(firstName) || !/^[\w\s]+$/.test(lastName) || !/^[\w\s]+$/.test(username)) {
       showError("Only letters, numbers, and spaces are allowed in text fields.");
       return;
     }
   
-
+    // Validación de contraseña con letras y números
     if (!/\d/.test(password) || !/[a-zA-Z]/.test(password)) {
       showError("Password must contain both letters and numbers.");
       return;
     }
-}  
+  
+    // Validación de edad
+    const birthdate = new Date(birthday);
+    const currentDate = new Date();
+    const age = currentDate.getFullYear() - birthdate.getFullYear();
+    const birthdateDay = birthdate.getDate();
+    const currentDay = currentDate.getDate();
+    
+    if (age < 18 || (age === 18 && currentDay < birthdateDay)) {
+      showError("You have to be 18 years old or older.");
+      return;
+    }
+  
+    // Validación de checkboxes
+    if (!newsletterCheckbox.checked || !termsCheckbox.checked) {
+      showError("Please agree to the terms and conditions.");
+      return;
+    }
+  
+    // Formulario válido, se puede enviar
+    alert("Form submitted successfully!");
+    document.getElementById("registration-form").reset();
+  }
+  
+  function showError(message) {
+    alert(message);
+  }
+  
+  document.addEventListener("DOMContentLoaded", function() {
+    flatpickr("#birthdate", {
+      dateFormat: "d-m-Y"
+    });
+  });
+  
+  document.getElementById("registration-form").addEventListener("submit", validateForm);
+  
