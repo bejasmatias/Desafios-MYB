992fd8e6d6935c0153619e748ca00f1c76ae2803
Matias Bejas <bejasmatias@gmail.com>
2023-06-07 14:00:41 -0300

    adding email field with validations, solving calendar issue

--- Clases/Clase6-Desafio3/script.js
+++ Clases/Clase6-Desafio3/script.js
@@ -1,8 +1,11 @@
+// Función de validación del formulario
 function validateForm(event) {
   event.preventDefault();
 
+  // Obtener los valores de los campos del formulario
   const firstName = document.getElementById("first-name").value;
   const lastName = document.getElementById("last-name").value;
+  const email = document.getElementById("email").value;
   const username = document.getElementById("username").value;
   const password = document.getElementById("password").value;
   const confirmPassword = document.getElementById("confirm-password").value;
@@ -10,21 +13,41 @@ function validateForm(event) {
   const newsletterCheckbox = document.getElementById("newsletter-checkbox");
   const termsCheckbox = document.getElementById("terms-checkbox");
 
-  // Validación de campos
+  // Validación de campos vacíos
   if (!firstName || !lastName || !username || !password || !confirmPassword || !birthday) {
     showError("Please fill in all fields.");
     return;
   }
 
-  // Validación de contraseñas
+  // Validación de coincidencia de contraseñas
   if (password !== confirmPassword) {
     showError("Passwords do not match.");
     return;
   }
 
   // Validación de caracteres especiales en campos de texto
-  if (!/^[\w\s]+$/.test(firstName) || !/^[\w\s]+$/.test(lastName) || !/^[\w\s]+$/.test(username)) {
+  if (!/^[\w\s]+$/.test(firstName) || !/^[\w\s]+$/.test(lastName)) {
     showError("Only letters, numbers, and spaces are allowed in text fields.");
+    return;
+  }
+
+  // Validación de coincidencia entre username y mail
+  if (username == email) {
+    showError("Email can't be the same as username.");
+    return;
+  }
+
+  // Validación del campo de correo electrónico
+  if (!email) {
+    showError("Please enter an email address.");
+    return;
+  }
+
+  // Expresión regular para validar el formato del correo electrónico
+  const emailRegex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+  if (!emailRegex.test(email)) {
+    showError("Please enter a valid email address.");
     return;
   }
 
@@ -40,7 +63,7 @@ function validateForm(event) {
   const age = currentDate.getFullYear() - birthdate.getFullYear();
   const birthdateDay = birthdate.getDate();
   const currentDay = currentDate.getDate();
-  
+
   if (age < 18 || (age === 18 && currentDay < birthdateDay)) {
     showError("You have to be 18 years old or older.");
     return;
@@ -58,13 +81,16 @@ function validateForm(event) {
 }
 
 // Mostrar un mensaje de error
-// Esta función se encarga de mostrar un mensaje de error utilizando la función "alert" del navegador.
-// Recibe un parámetro "message" que representa el mensaje de error a mostrar.
 function showError(message) {
   alert(message);
 }
 
+// Configurar el calendario para seleccionar la fecha de nacimiento
+document.addEventListener("DOMContentLoaded", function() {
+  flatpickr("#birthdate", {
+    dateFormat: "d-m-Y"
+  });
+});
+
 // Agregar un evento de escucha al formulario de registro
-// Este evento de escucha se activa cuando se envía el formulario de registro.
-// Al activarse, llama a la función "validateForm" para realizar la validación del formulario.
 document.getElementById("registration-form").addEventListener("submit", validateForm);
